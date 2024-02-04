cba2cb86e017fbd6beb0ddc2ff5501fac6e91ba9
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2024-02-04 14:38:43 -0500

    orthographe

--- test/views/index-lab0.test.ts
+++ test/views/index-lab0.test.ts
@@ -16,7 +16,7 @@ describe('views/index.pug', () => {
 
 describe('views/stats.pug', () => {
 
-  it("devrait ne pas avoir la colonne ration en commentaire ", () => {
+  it("devrait ne pas avoir la colonne ratio en commentaire ", () => {
     expect(contentClassement.includes(`//- td(style="text-align: right; font-family: monospace") #{joueur.ratio.toFixed(8)}`)).toBeFalsy();
     expect(contentClassement.includes(`  td(style="text-align: right; font-family: monospace") #{joueur.ratio.toFixed(8)}`)).toBeTruthy();
   });
