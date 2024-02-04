5a13fe9fc946918d0f10bfb03a71dfa11791d218
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2024-02-04 14:22:17 -0500

    message en français

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -3,7 +3,7 @@ import { assert } from 'console';
 import 'jest-extended';
 
 describe('redemarrerJeu.test.ts', () => {
-  it("should implement test", async () => {
+  it("devrait implémenter test", async () => {
     throw new Error("Ce test n'a pas été défini")
   });
 });
